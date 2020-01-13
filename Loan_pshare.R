69ae36a36211acb631c8c3f70803cb45aac8c2e2
AbijahDionysius <59437035+AbijahDionysius@users.noreply.github.com>
2020-01-13 16:45:30 +0530

    Add files via upload

---
+++ Loan_pshare.R
@@ -0,0 +1,29 @@
+getwd();
+
+Loands=read.csv("Loan Processing.csv",header = TRUE);
+names(Loands);
+attach(Loands);
+Status=factor(Status,levels=c(0,1),labels=c("Reject Loan","Accept Loan"));
+Status;
+Model=glm(Status~Credit.Score,data=Loands,family=binomial)
+library(lmtest)
+lrtest(Model)
+library(pscl)
+# Compute various Pseudo R squares 
+pR2(Model)
+summary(Model)
+confint(Model)
+exp(coef(Model))
+#exp(coef(Model))/(1-exp(coef(Model)))
+#exp(confint(Model))
+predict=predict(Model,type="response")
+library(Deducer)
+rocplot(Model)
+
+pred = fitted(Model)
+pred
+data.frame(Status,pred)
+# Cut off points 0.50
+gg1 = floor(pred)
+table(Actual=Status,Prediction=gg1)
+
